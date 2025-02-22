e0a4c51d730f8d212d14ccc8b131ad479adb7d4b
Aisan14 <sarsen.aisan@mail.ru>
2025-02-22 13:05:21 +0500

    Тестовый комит 2

--- src/Practice.java
+++ src/Practice.java
@@ -3,5 +3,8 @@ import java.util.Scanner;
 public class Practice {
     public static void main(String[] args) {
         System.out.println("hello world");
+        Scanner sc = new Scanner(System.in);
+        sc.nextLine();
+
     }
 }
